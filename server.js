d71b9820645b7a3d91c1834ed9ad45a24ab71b75
Iyana Marquez <yayakix@gmail.com>
2022-06-16 13:51:52 -0400

    server setup and mongoose connection

---
+++ server.js
@@ -0,0 +1,88 @@
+// get env variables
+require("dotenv").config();
+const { PORT = 4000, MONGODB_URL } = process.env;
+
+
+// require express
+const express = require("express");
+const app = express();
+
+// get mongo schemas
+const Items = require("./models/item");
+const itemSeed = require("./models/seed");
+
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const Item = require("./models/item");
+
+
+
+mongoose.connect(MONGODB_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+mongoose.connection
+  .on("open", () => console.log("You are connected to mongoose"))
+  .on("close", () => console.log("You are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
+
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); 
+
+
+app.get("/", (req, res) => {
+  res.send("root page");
+});
+// index page
+app.get("/items", async (req, res) => {
+  try {
+    // send all people
+    res.json(await Items.find({}));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+// item create route
+app.post("/items", async (req, res) => {
+  try {
+    // send all people
+    res.json(await Items.create(req.body));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+// item delte route
+app.delete("/items/:id", async (req, res) => {
+  try {
+    // send all people
+    res.json(await Item.findByIdAndRemove(req.params.id));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+// items UPDATE ROUTE
+app.put("/items/:id", async (req, res) => {
+  try {
+    // send all people
+    res.json(
+      await Item.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+app.get("/seed", (req, res) => {
+  res.json(itemSeed);
+});
+
+// console.log(itemSeed)
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+
